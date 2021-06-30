cf73f7709a894c9facf72d253ec179ae5d0b436c
Naomi Keller <thatstraveling@gmail.com>
2021-06-30 15:24:42 -0400

    update to run batch

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
-# /bin/sh
+# Create file '.heroku/run.sh' containing bash commands
+echo 'echo "hello world"' >.heroku/run.sh
+
+heroku buildpacks:set https://github.com/niteoweb/heroku-buildpack-shell.git
 
 until node mcbot.js; do
     echo "Bot stopped with exit code $?. Restarting..." >&2
