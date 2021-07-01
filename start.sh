4d627639ef70871c9db392d2994c020dfc3505a2
Naomi Keller <thatstraveling@gmail.com>
2021-07-01 16:17:35 -0400

    no THIS is the last attempt

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo 'echo "hello world"' >.heroku/run.sh
 heroku buildpacks:set https://github.com/niteoweb/heroku-buildpack-shell.git
 
 until node mcbot.js; do
-    echo "Bot stopped with exit code $?. Restarting..." >&2.heroku/start.sh
+    echo "Bot stopped with exit code $?. Restarting..." >&2
     sleep 1
 done
 
