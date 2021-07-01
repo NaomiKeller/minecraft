ac012476590eaf4b638ca553be24d4909f4c2429
Naomi Keller <thatstraveling@gmail.com>
2021-07-01 16:14:22 -0400

    ditto

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 # Create file '.heroku/run.sh' containing bash commands
 until node mcbot.js; do
-    echo "Bot stopped with exit code $?. Restarting..." >.heroku/start.sh
+    echo "Bot stopped with exit code $?. Restarting..." >&2.heroku/start.sh
     sleep 1
 done
 
