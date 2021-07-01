d2297862dd51f3b8648ba91152ceb9d9f71decfc
Naomi Keller <thatstraveling@gmail.com>
2021-07-01 19:53:31 -0400

    rollback

--- start.sh
+++ start.sh
@@ -1,12 +1,5 @@
-# Create file '.heroku/run.sh' containing bash commands
-echo 'echo "hello world"' >.heroku/start.sh
-
-heroku buildpacks:set https://github.com/niteoweb/heroku-buildpack-shell.git
-
+# /bin/sh
 until node mcbot.js; do
     echo "Bot stopped with exit code $?. Restarting..." >&2
     sleep 1
 done
-
-
-
