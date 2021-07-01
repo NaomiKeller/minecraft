c9ffb2588fe923e7b58b3935494220d8a01d517b
Naomi Keller <thatstraveling@gmail.com>
2021-07-01 16:13:31 -0400

    ditto

--- start.sh
+++ start.sh
@@ -2,9 +2,7 @@
 until node mcbot.js; do
     echo "Bot stopped with exit code $?. Restarting..." >.heroku/start.sh
     sleep 1
-
-    heroku buildpacks:set https://github.com/niteoweb/heroku-buildpack-shell.git
 done
 
-
+heroku buildpacks:set https://github.com/niteoweb/heroku-buildpack-shell.git
 
